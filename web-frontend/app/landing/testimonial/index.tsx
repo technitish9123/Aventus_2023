9df37a55bd422fc6eec89196775cf6662c27a547
Nitish Kumar <kumarnitish072001@gmail.com>
2023-06-27 11:55:18 +0530

    :fix

--- web-frontend/app/landing/testimonial/index.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @next/next/no-img-element */
-import LogoForbes from "./forbes.svg";
-import LogoTechcrunch from "./techcrunch.svg";
-import LogoWired from "./wired.svg";
-import LogoInc42 from "./inc42.svg";
-
-export default function Testimonial() {
-  return <></>;
-}
